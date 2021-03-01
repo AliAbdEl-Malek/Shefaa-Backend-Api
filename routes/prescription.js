37457798967136ed181d0ffa376dccd695b6c5a1
8ahmedmohamed <ahmedkhan.am8@gmail.com>
2021-03-01 23:02:01 +0200

    prescription

--- routes/prescription.js
+++ routes/prescription.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const User = require('../models/user')
+
 const prescription = require('../models/prescription')
 
 const multer = require('multer');
@@ -20,12 +22,22 @@ const upload = multer({ storage: storage })
 // upload prescription image 
 router.post('/:id', upload.single('prescriptionURL'), (req, res) => {
 
+    let userData;
     let id = req.params.id
     console.log(id)
     console.log("req.File: ", req.file)
 
     let prescription_URL = req.file.path
     console.log("req.files.path:", req.file.path)
+
+    User.findOne({ _id: req.params.id }, (err, user) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log(user)
+            userData = user
+        }
+    })
 
     prescription.create({
         userID: req.params.id,
@@ -34,11 +46,23 @@ router.post('/:id', upload.single('prescriptionURL'), (req, res) => {
         if (err) {
             res.status(500).send({ "Data": err, "message": "Failed in posting a prescription...!", "status": false })
         } else {
-            res.status(200).send({ "Data": data, "message": "Prescription uploaded successfully...!", "status": true })
+            res.status(200).send({ "Data": {"data":data , "user":userData} , "message": "Prescription uploaded successfully...!", "status": true })
         }
 
     })
 
+})
+
+
+//Get All prescriptions
+router.get('/', (req, res) => {
+    prescription.find({}, (err, data) => {
+        if (err) {
+            res.status(500).send({ "Data": err, "message": "Failed in getting prescriptions data ...!", "status": false })
+        } else {
+            res.status(200).send({ "Data": data, "message": "All prescriptions retrieved Successfully..!", "status": true })
+        }
+    })
 })
 
 
